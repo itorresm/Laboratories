838b1d211fa87a20d8a5574b54c3b53607e7c24a
Ibrahim Torres <itorresm@unal.edu.co>
2019-11-02 10:51:51 -0500

    Added changes to solenoide

--- M_Electro/Solonoide/solenoide.cpp
+++ M_Electro/Solonoide/solenoide.cpp
@@ -6,12 +6,12 @@ int main (void)
 {
   const int N = 10;
   std::ofstream fout("solenoide.txt");
-  //fout << "Corriente (A)" << "\t" << "Campo (G)" << "\n";
+  fout << "# Corriente (A)" << "\t" << "Campo (G)" << "\n";
   std::array<double, N> campo = {15.0, 29.2, 43.5,
 				 56.6, 77.6, 87.8, 100.9, 115.2, 133.4, 148.1};
   for (int ii = 0; ii < N; ++ii)
     {
-      fout << ii*0.3 + 0.3 << "\t" << campo[ii] << "\n";
+      fout << "  " << ii*0.3 + 0.3 << "\t        " << campo[ii] << "\n";
     }
   fout.close();
   return 0;
