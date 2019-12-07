baed83d163812e81bfafe65c086b2c9418628762
Ibrahim Torres <itorresm@unal.edu.co>
2019-12-06 21:43:41 -0500

    Added changes to proyecto

--- proyecto_prog/proyecto.cpp
+++ proyecto_prog/proyecto.cpp
@@ -16,7 +16,9 @@ const double M = 1.0;
 const double Em = 1/std::pow(M_E, 2);
 const double TF = 45.0;
 const double DT = 0.1;
-const double H = 0.01;
+const double H = 0.001;
+const double BA = -0.3920;
+const double BB = -0.3770;
 
 void initial_conditions(Particle & p, double b);
 void force(Particle & p, double t, const std::vector<double> & dV);
@@ -25,11 +27,15 @@ double potential(Particle & p);
 void grad_potential(Particle & p, std::vector<double> & dV);
 double angle(Particle & p);
 void file(const std::vector<double> & rx, const std::vector<double> & ry,
-	  std::string fname);
+	  const std::vector<double> & ang, double & b, std::string fname);
 void save(Particle & p,
 	  std::vector<double> & rx, std::vector<double> & ry,
 	  std::vector<double> & vx, std::vector<double> & vy,
-	  std::vector<double> & fx, std::vector<double> & fy);
+	  std::vector<double> & fx, std::vector<double> & fy,
+	  std::vector<double> & ang);
+void phi(std::vector<double> & ang);
+void start_gnuplot_ang(void);
+void gnuplot_ang(void);
 	  
 int main(void)
 {
@@ -42,12 +48,13 @@ int main(void)
   std::vector<double> vy = {0};
   std::vector<double> fx = {0};
   std::vector<double> fy = {0};
+  std::vector<double> ang = {0};
   std::vector<double> dV(2);
 
   std::string name;
   Particle p1;
   int count = 1;
-  for(b = -0.4; b < -0.3; b += H)
+  for(b = BA; b < BB; b += H)
     {
       name = "trayectoria_" + std::to_string(count) + ".txt";
       
@@ -57,17 +64,21 @@ int main(void)
 	  grad_potential(p1, dV);
 	  force(p1, t, dV);
 	  evolve(p1, t, DT);
-	  save(p1, rx, ry, vx, vy, fx, fy);
+	  save(p1, rx, ry, vx, vy, fx, fy, ang);
 	}
-      file(rx, ry, name);
+      //phi(ang);
+      file(rx, ry, ang, b, name);
       rx = {0};
       ry = {0};
       vx = {0};
       vy = {0};
       fx = {0};
       fy = {0};
+      ang = {0};
       ++count;
     }
+  start_gnuplot_ang();
+  gnuplot_ang();
   return 0;
 }
 
@@ -110,13 +121,25 @@ void grad_potential(Particle & p, std::vector<double> & dV)
 
 double angle(Particle & p)
 {
-  return (std::acos(p.Vx/std::sqrt(2.0*(0.260*Em-potential(p))/M)))*180/M_PI;
+  if((p.Vx > 0 and p.Vy > 0) or (p.Vx < 0 and p.Vy < 0))
+    {
+      return (std::acos(p.Vx/std::sqrt(2.0*(0.260*Em-potential(p))/M)))/1.0;
+    }
+  else if((p.Vx > 0 and p.Vy < 0) or (p.Vx < 0 and p.Vy > 0))
+    {
+      return (-std::acos(p.Vx/std::sqrt(2.0*(0.260*Em-potential(p))/M)))/1.0;
+    }
+  else
+    {
+      return (std::acos(p.Vx/std::sqrt(2.0*(0.260*Em-potential(p))/M)))/1.0;
+    }
 }
 
 void save(Particle & p,
 	  std::vector<double> & rx, std::vector<double> & ry,
 	  std::vector<double> & vx, std::vector<double> & vy,
-	  std::vector<double> & fx, std::vector<double> & fy)
+	  std::vector<double> & fx, std::vector<double> & fy,
+	  std::vector<double> & ang)
 {
   rx.push_back(p.Rx);
   ry.push_back(p.Ry);
@@ -124,15 +147,62 @@ void save(Particle & p,
   vy.push_back(p.Vx);
   fx.push_back(p.Fx);
   fy.push_back(p.Fy);
+  ang.push_back(angle(p));
 }
 
 void file(const std::vector<double> & rx, const std::vector<double> & ry,
-	  std::string fname)
+	  const std::vector<double> & ang, double & b, std::string fname)
 {
   std::ofstream fout(fname);
   for(int ii = 0; ii < int(rx.size()); ++ii)
     {
-      fout << ii*0.1 << "\t" << rx[ii] << "\t" << ry[ii] << "\n";   
+      fout << ii*0.1 << "\t" << rx[ii] << "\t" << ry[ii] << "\t"
+	   << ang[ii] << "\t" << b <<  "\n";   
     }
   fout.close();
 }
+
+void start_gnuplot_ang(void)
+{
+  std::cout << "reset" << "\n"
+	    << "set term pdf" << "\n"
+	    << "set out 'angulo.pdf'" << "\n"
+	    << "unset key" << "\n"
+	    << "set xrange [0:45]" << "\n"
+	    << "set yrange [-3:3]" << "\n"
+	    << "set xlabel 'b'" << "\n"
+    	    << "set ylabel '{/Symbol f}(rad)'" << "\n";
+}
+
+void gnuplot_ang(void)
+{
+  std::cout << "plot"; 
+  for(int ii = 1; ii <= int((BB-BA)/H); ++ii)
+    {
+      std::cout << " 'trayectoria_" << ii << ".txt'"
+		<< "u 1:4 w p ls -1  lw 0.001,";
+    }
+  std::cout <<  "\n";
+}
+
+void phi(std::vector<double> & ang)
+{
+  std::ofstream fout("angulo.txt");
+  for(int ii = 0; ii <= int((BB-BA)/H); ++ii)
+    {
+      fout << ii*H + BA << "\t" << ang[ii];
+    }
+  fout.close();
+}
+
+//          Gnuplot para trayectorias
+//          << "f(x,y) = x**2*y**2*exp(-(x**2+y**2))" << "\n"
+// 	    << "set contour" << "\n"
+// 	    << "set cntrparam levels incre 0.03,0.01,0.13" << "\n"
+// 	    << "set view map" << "\n"
+// 	    << "set isosample 100,100" << "\n"
+// 	    << "unset key" << "\n"
+// 	    << "set xrange [-3:3]" << "\n"
+// 	    << "set yrange [-3:3]" << "\n"
+//          << "set xlabel 'x' <<  "\n"
+//          << "set ylabel 'y' <<  "\n";
